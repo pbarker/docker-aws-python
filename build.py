43bb73b74e01449c89d232f20737dc63e3064235
Chris Childress <chris@tonzof.com>
2015-07-28 12:39:42 -0600

    Notes for Pat on args

--- build.py
+++ build.py
@@ -6,9 +6,25 @@ import boto.route53
 import argparse
 
 
-# #
-
+##
 #Arguments
+#
+# Patrick can you please support all of the following arguments? with the indented value on the following row is what they will default to if nothing is passed?
+#
+# region <string> http://docs.aws.amazon.com/general/latest/gr/rande.html#ec2_region (Under region column, us-east-1, us-west-1, etc)
+#     us-west-1
+# cname <string> Any string, the script can be smart enough to point the c record at the a name EX: sub.mydomain.com
+#     null
+# size <string> Here are all the amazon instance sizes, http://aws.amazon.com/ec2/instance-types/, I would probably only want to support the general purpose instances for now Ex: t2.micro, t2.small, etc
+#     t2.micro
+# securitygroup <string> We should build in support for this, just accepts the security group ID to build this server in
+#     null
+# os <string> I would dumb this down and only support redhat, amazon, ubuntu, suse (all 64)
+#     amazon
+# drivesize <int> Gb of the main /dev/xvda volume
+#     8
+#
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-n","--name",
                     help="name of instance")
@@ -33,15 +49,20 @@ logger.setLevel(logging.DEBUG)
 
 # #
 # Records
-a = records.getAAlias()
-c = records.getCAlias()
-ip = records.getPublicIp()
+# a = records.getAAlias()
+# c = records.getCAlias()
+# ip = records.getPublicIp()
 
 # #
-# AWS API
+# START AWS Code here
 # Pick up here
+# These need to be environmental variables set
+# TODO we need a check here
 os.environ['AWS_ACCESS_KEY_ID'] = config.getConfig('awsKey')
 os.environ['AWS_SECRET_ACCESS_KEY'] = config.getConfig('awsSecret')
+
+
+
 conn = boto.route53.connect_to_region('us-west-2')
 zone = conn.get_zone(config.getConfig('domain'));
 # A
